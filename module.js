9f40d7c922c6d82b5187645a4ce3d69fb6f0c82d
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-01-04 18:49:20 +0530

    Create module.js

---
+++ module.js
@@ -0,0 +1,11 @@
+
+const operatingSystem = require('os');
+
+console.log(operatingSystem.type());
+console.log(operatingSystem.platform());
+
+const crypto = {
+    price: 1500,
+    buy: true
+}
+console.log(crypto);
