f86cd87857dba8dada2115929c56b352e936c3bb
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-07-13 14:05:55 +0530

    Delete https01.js

--- https01.js
+++
@@ -1,8 +0,0 @@
-const urlObj = new URL('https://example.com:8080/example-path');
-const hostname = urlObj.hostname;
-const protocol = urlObj.protocol;
-const port = urlObj.port;
-
-console.log(hostname);
-console.log(protocol);
-console.log(port);
