536d2c35bacc19ffaf44530686a92973e5f73fae
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-03-13 01:50:32 +0530

    Update index.js

--- 2025/Node&Express/index.js
+++ 2025/Node&Express/index.js
@@ -5,9 +5,36 @@ app.listen(3000, () => {
     console.log(`Successfully connected on port 3000`);
 });
 
-app.get("/", (req, res) => {
-    res.send("<h1>Hello World</h1>");
+app.get("/object", (req, res) => {
+    res.send(
+        {
+            name: "Crypto",
+            year: 2020
+        }
+    );
 })
+
+app.get("/array", (req, res) => {
+    res.send(
+        ["BNB", "ETH", "SOL"]
+    )
+});
+
+app.get("/json", (req, res) => {
+    res.send({
+        name: "Crypto",
+        year: 2025
+    })
+});
+
+app.get("/redirect", (req, res) => {
+    res.redirect("https://google.com")
+});
+
+// download the pdf file
+app.get("/download", (req, res) => {
+    res.download("./exchangelist.pdf")
+});
 
 // http://localhost:3000/user/10
 app.get("/user/:id", (req, res) => {
